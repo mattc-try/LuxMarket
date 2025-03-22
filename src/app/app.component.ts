57f427b2d7d842eb14a88a1a32ed9bee90cbb4ea
mattc-try <misterkrakyze.mc@gmail.com>
2025-03-22 01:11:55 +0100

    fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { ProductService } from './product.service';
 import { Product } from './product';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
